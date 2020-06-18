8bb50e38dc9145785e2ad58c73875e92905c06db
Liam Wynn <wynnliam@pdx.edu>
2020-06-18 14:32:24 -0700

    Added basic eval function

--- isidore.hs
+++ isidore.hs
@@ -2,13 +2,19 @@
 
 {-|
 	Isidore is a simple calculator for arithmetic expressions.
-	It will support addition, subtraction, and multiplication
-	of simple integer numbers.
+	It will support addition, subtraction, multiplication,
+	and division of simple integer numbers.
 
 	The idea is that you can see how the evaluator computes
 	each step.
 -}
 
 data Term = Const Int | Add Term Term | Sub Term Term | Mul Term Term
-data Output = String
-data State = State Output Term
+--data Output = String
+--data State = State Output Term
+
+eval :: Term -> Int
+eval (Const a) = a
+eval (Add x y) = (eval x) + (eval y)
+eval (Sub x y) = (eval x) - (eval y)
+eval (Mul x y) = (eval x) * (eval y)
