f49541a9e559e7de2f0e5cc538285fed4ca3aca3
Liam Wynn <wynnliam@pdx.edu>
2020-06-18 16:24:05 -0700

    Implemented new eval for Const and Div

--- isidore.hs
+++ isidore.hs
@@ -9,20 +9,27 @@
 	each step.
 -}
 
-data Term = Const Int | Add Term Term | Sub Term Term | Mul Term Term | Div Term Term
+data Term = Const Int | Add Term Term | Sub Term Term | Mul Term Term | Div Term Term deriving (Show)
 data Output = Exception String | Result Int deriving (Show)
---data State = State Output Term
+type State = (String, Output)
 
-eval :: Term -> Output
-eval (Const a) = Result a
-eval (Div x y) = tryOp (eval x) (eval y) (div)
-eval (Add x y) = tryOp (eval x) (eval y) (+)
-eval (Sub x y) = tryOp (eval x) (eval y) (-)
-eval (Mul x y) = tryOp (eval x) (eval y) (*)
-  
+eval :: Term -> State
+eval (Const a) = ((line (Const a) (Result a)), (Result a))
+eval (Div x y) =
+  let a = eval x
+      b = eval y
+      res = tryOp (snd a) (snd b) (div)
+  in ((fst a) ++ (fst b) ++ (line (Div x y) res), res)
+
+line :: Term -> Output -> String
+line t q = (show t) ++ " = " ++ show q ++ "\n"
+--eval (Add x y) = tryOp (eval x) (eval y) (+)
+--eval (Sub x y) = tryOp (eval x) (eval y) (-)
+--eval (Mul x y) = tryOp (eval x) (eval y) (*)
+--  
 tryOp :: Output -> Output -> (Int -> Int -> Int) -> Output
 tryOp (Exception p) _ _ = Exception p
 tryOp _ (Exception p) _ = Exception p
-tryOp (Result 0)  _ _ = Exception "Divided by zero"
-tryOp _ (Result 0) _ = Exception "Divided by zero"
+tryOp (Result 0)  _ (div) = Exception "Divided by zero"
+tryOp _ (Result 0) (div) = Exception "Divided by zero"
 tryOp (Result x) (Result y) op = Result (op x y)
